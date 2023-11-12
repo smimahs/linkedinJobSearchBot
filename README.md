c222ef1d30c25978888c36f6dc8d865e0dc64353
Shamim Sanisales <shamim.sanisales@nexxyolabs.com>
2023-11-12 12:22:57 +0330

    add project files

---
+++ README.md
@@ -0,0 +1,28 @@
+### Quick Start Guide :
+
+<br>
+
+1. Create a bot using BotFather :
+<br>
+
+2. Create a *.env* file in the main directory and add your credentials : 
+
+Create a *.env* file and put the `BOT_TOKEN` you got from the BotFather
+
+<br>
+
+3. Install prerequisites using the requirements.txt file.
+Navigate to the project directory and type the following command in the terminal :
+
+```
+pip install -r requirements.txt
+```
+
+<br>
+
+4. Run the bot :
+
+```
+python bot.py
+```
+<br>
---
+++ src/tgbot/utilities/chat_helper.py
@@ -0,0 +1,26 @@
+msgs = {
+    "check": [
+        "Up and running... 👍",
+        "All Good, no worries 👍",
+        "Thanks for checking 😊",
+    ],
+    "help": [
+        "I search linkedin for *Jobs💼* posted last week and send them in chat.\nI also have some tips for you, send me a /tips command to view them.",
+        "I can tell you what *Jobs 💼* posted on linkedin last week.\nYou can send me a /tips command to view some tips.",
+    ],
+    "tips": [
+        "Landing a job could be challenging if you are not well prepared, so, here are some tips:\n\n🔰 _Follow this guide to setup your GitHub profile_. [HERE](https://www.sitepoint.com/github-profile-readme/)\n🔰 Follow this guide to setup your LinkedIn profile. [HERE](https://www.linkedin.com/business/talent/blog/product-tips/linkedin-profile-summaries-that-we-love-and-how-to-boost-your-own)",
+        "Finding a job might be difficult if you are not properly prepared, so here are some pointers.\n\n🔰 _Setup your GitHub profile by following this tutorial_. [HERE](https://www.sitepoint.com/github-profile-readme/)\n🔰 To set up your LinkedIn profile, follow this tutorial.[HERE](https://www.linkedin.com/business/talent/blog/product-tips/linkedin-profile-summaries-that-we-love-and-how-to-boost-your-own)",
+    ],
+    "help_admins": (
+        "*You are an Admin | Owner. Here are your control commands.*\n\n"
+        "• /check = _Check if bot is running_.\n"
+        "• /ljobs = _Searches linkedin for jobs default is BI Data Analyst Egypt_.\n"
+        "• /ljobs job title location = _Searches linkedin for job title in location_.\n"
+        "• /echo = _Echos back what ever you send it_.\n"
+        "• /addgroup group id = _Adds group to the allow list_.\n"
+        "• /rmgroup group id = _Removes group from allow list_.\n"
+        "• /getgroup = _Returns back a list of all allowed group(s) id(s) in the database_.\n"
+        "• /getgroup group id = _Returns back the group id if it exists in the database_."
+    ),
+}
